481eb712a51d0702cfd1823da89fbfaa21851f1a
pedro-crb <pedro.c.rib123@gmail.com>
2024-05-23 15:24:48 -0300

    fixed importing issue

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='NeuralFoil',
+    name='neuralfoil-standalone',
     version="0.2.3",
-    description='NeuralFoil is an airfoil aerodynamics analysis tool using physics-informed machine learning, '
-                'in pure Python/NumPy.',
+    description='neuralfoil-standalone is a fork of NeuralFoil without the explicit AeroSandbox dependency',
     long_description_content_type='text/markdown',
     packages=["neuralfoil_standalone"],
     python_requires='>=3.11',
